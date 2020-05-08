db990ea52b208fceea23dac0fcee86f162b619bc
Frank Uzoka <Frankuzoka@hotmail.com>
2020-05-07 20:51:17 -0400

    Add logic

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class App {
 
 
@@ -7,6 +9,22 @@ public class App {
 
     static void logic(){
 
+        System.out.println("Enter three integers:\n");
+
+        Scanner side1 = new Scanner(System.in);
+        Scanner side2 = new Scanner(System.in);
+        Scanner side3 = new Scanner(System.in);
+
+        System.out.print("Side 1:");
+        int int1 = side1.nextInt();
+
+        System.out.print("Side 2:");
+        int int2 = side1.nextInt();
+
+        while (int1 > int2)
+            System.out.println( int2 + " is smaller than 4.  Try again.\n");
+        System.out.println("Side 2");
+        int2 = side1.nextInt();
 
 
     }
